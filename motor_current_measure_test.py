85cf8ee5d89335a70897956168fc79a51849efaa
michaelyoyo <michael.yoyo@gmail.com>
2018-11-01 18:50:28 -0500

    turn off leds on init

--- motor_current_measure_test.py
+++ motor_current_measure_test.py
@@ -11,13 +11,16 @@ motor_speed = 0
 pwm.set_pwm_freq(600)
 GAIN = 1
 pwm.set_pwm(0,4096,4096)
-
+pwm.set_pwm(1,4096,4096)
+pwm.set_pwm(2,4096,4096)
+pwm.set_pwm(3,4096,4096)
+pwm.set_pwm(4,4096,4096)
 print('Reading ADS1x15 values, press Ctrl-C to quit...')
 print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
 print('-' * 37)
 while True:
 	print motor_speed%20
-	if(motor_speed % 20):
+	if((motor_speed % 20) == 0):
 		pwm.set_pwm(5, int(4096-motor_speed*409.6), 4096)
 		pwm.set_pwm(3, 1024, 4096)
 		print ping
