08a8d7181549e6a9a3a8d6624d14d961e9971f3d
michaelyoyo <michael.yoyo@gmail.com>
2018-11-01 18:44:04 -0500

    changed pwm outputs

--- motor_current_measure_test.py
+++ motor_current_measure_test.py
@@ -15,10 +15,12 @@ print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
 print('-' * 37)
 while True:
 	if(motor_speed % 20):
-		pwm.set_pwm(5, (4096-motor_speed*409.6), 4096)
-		pwm.set_pwm(4, 2048, 4096)
+		pwm.set_pwm(5, int(4096-motor_speed*409.6), 4096)
+		pwm.set_pwm(3, 1024, 4096)
+		print ping
 	else:
-		pwm.set_pwm(4, 4096, 4096)
+		pwm.set_pwm(3, 256, 4096)
+		print pong
 	values = [0]*4
 	for i in range(4):
 		values[i] = adc.read_adc(i, gain=GAIN)
