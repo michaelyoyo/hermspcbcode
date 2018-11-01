9ecbfa67ebd3d9187a64c803b7f23ba885d6ebbc
michaelyoyo <michael.yoyo@gmail.com>
2018-11-01 18:41:28 -0500

    bug fix

--- motor_current_measure_test.py
+++ motor_current_measure_test.py
@@ -15,10 +15,10 @@ print('| {0:>6} | {1:>6} | {2:>6} | {3:>6} |'.format(*range(4)))
 print('-' * 37)
 while True:
 	if(motor_speed % 20):
-		pwm.set_pwm_freq(5, (4096-motor_speed*409.6), 4096)
-		pwm.set_pwm_freq(4, 2048, 4096)
+		pwm.set_pwm(5, (4096-motor_speed*409.6), 4096)
+		pwm.set_pwm(4, 2048, 4096)
 	else:
-		pwm.set_pwm_freq(4, 4096, 4096)
+		pwm.set_pwm(4, 4096, 4096)
 	values = [0]*4
 	for i in range(4):
 		values[i] = adc.read_adc(i, gain=GAIN)
