3f30744951a8f6dc047751fd1970a89ec5c32199
michaelyoyo <michael.yoyo@gmail.com>
2018-11-01 19:01:07 -0500

    fixed it! maybe

--- motor_current_measure_test.py
+++ motor_current_measure_test.py
@@ -5,31 +5,31 @@ import Adafruit_PCA9685
 
 # Initialise the PCA9685 using the default address (0x40).
 pwm = Adafruit_PCA9685.PCA9685()
-#adc = Adafruit_ADS1x15.ADS1115()
+adc = Adafruit_ADS1x15.ADS1115()
 
 motor_speed = 0
 pwm.set_pwm_freq(600)
 GAIN = 1
-pwm.set_pwm(0,0,4096)
-pwm.set_pwm(1,0,4096)
-pwm.set_pwm(2,0,4096)
-pwm.set_pwm(3,0,4096)
-pwm.set_pwm(4,0,4096)
+pwm.set_pwm(0,0,4095)
+pwm.set_pwm(1,0,4095)
+pwm.set_pwm(2,0,4095)
+pwm.set_pwm(3,0,4095)
+pwm.set_pwm(4,0,4095)
 print('Reading ADS1x15 values, press Ctrl-C to quit...')
 print('| {0:>6} | {1:>6} |'.format(*range(2)))
 print('-' * 37)
 while True:
 	print motor_speed%20
 	if((motor_speed % 20) == 0):
-		pwm.set_pwm(5, int(4096-motor_speed*409.6), 4096)
-		pwm.set_pwm(3, 1024, 4096)
+		pwm.set_pwm(5, int(4096-motor_speed*409.6), 4095)
+		pwm.set_pwm(3, 1024, 4095)
 		print "ping"
 	else:
-		pwm.set_pwm(3, 256, 4096)
+		pwm.set_pwm(3, 256, 4095)
 		print "pong"
 	values = [0]*2
-#	for i in range(2):
-#		values[i] = adc.read_adc(i, gain=GAIN)
+	for i in range(2):
+		values[i] = adc.read_adc(i, gain=GAIN)
 	print('| {0:>6} | {1:>6} |'.format(*values))
 	motor_speed += 5
 	if(motor_speed == 100):
