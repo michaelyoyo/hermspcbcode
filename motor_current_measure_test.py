62292439c023528737300ba2f5533d70051ce596
michaelyoyo <michael.yoyo@gmail.com>
2018-11-03 10:43:38 -0500

    fixed indentation

--- motor_current_measure_test.py
+++ motor_current_measure_test.py
@@ -19,21 +19,21 @@ print('Reading ADS1x15 values, press Ctrl-C to quit...')
 print('| {0:>6} | {1:>6} |'.format(*range(2)))
 print('-' * 37)
 while True:
-	# print motor_speed%20
-	if((motor_speed % 20) == 0):
-		pwm_start = int(4096-motor_speed*409.6)
+    # print motor_speed%20
+    if((motor_speed % 20) == 0):
+        pwm_start = int(4096-motor_speed*409.6)
         pwm.set_pwm(8, pwm_start, 4095)
         print pwm_start
-		pwm.set_pwm(3, 1024, 4095)
-		# print "ping"
-	else:
-		pwm.set_pwm(3, 0, 4095)
-		# print "pong"
-	values = [0]*2
-	for i in range(2):
-		values[i] = adc.read_adc(i, gain=GAIN)
-	print('| {0:>6} | {1:>6} |'.format(*values))
-	motor_speed += 5
-	if(motor_speed == 100):
-		motor_speed = 0
-	time.sleep(0.25)
+        pwm.set_pwm(3, 1024, 4095)
+        # print "ping"
+    else:
+        pwm.set_pwm(3, 0, 4095)
+        # print "pong"
+    values = [0]*2
+    for i in range(2):
+        values[i] = adc.read_adc(i, gain=GAIN)
+    print('| {0:>6} | {1:>6} |'.format(*values))
+    motor_speed += 5
+    if(motor_speed == 100):
+        motor_speed = 0
+    time.sleep(0.25)
